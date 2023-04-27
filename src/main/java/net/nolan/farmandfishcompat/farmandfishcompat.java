84dc331ecc33df897200cfb4494eb52fb7070e0a
NolanHewitt <Nolanhewitt@aol.com>
2023-04-27 04:27:17 -0500

    Initial commit

---
+++ src/main/java/net/nolan/farmandfishcompat/farmandfishcompat.java
@@ -0,0 +1,15 @@
+package net.nolan.farmandfishcompat;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class farmandfishcompat implements ModInitializer {
+	public static final String MOD_ID = "farmandfishcompat";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nolan/farmandfishcompat/farmandfishcompatClient.java
@@ -0,0 +1,10 @@
+package net.nolan.farmandfishcompat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class farmandfishcompatClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
